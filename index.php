5923425add2b5a16efb4a371b90039c18bc467f3
Mazino Ukah <ewomaukah@yahoo.com>
2019-05-20 08:47:54 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+require __DIR__ . "/./vendor/autoload.php";
+
+// throw new Exception('sjdsk');
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use Twilio\Twiml;
+use BotMan\Drivers\Web\WebDriver;
+
+$config = [
+    'twilio' => [
+        'token' => '8da346ce4f8671d62e1b3d010d257886',
+    ]
+];
+
+// Load the driver(s) you want to use
+DriverManager::loadDriver(\BotMan\Drivers\Twilio\TwilioMessageDriver::class); // Twilio driver
+DriverManager::loadDriver(WebDriver::class); // Web driver
+
+
+// Create an instance
+$botman = BotManFactory::create($config);
+
+// Give the bot something to listen for.
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('never give up baby');
+});
+
+
+
+$botman->fallback(function($bot) {
+
+   $bot->reply('dksd skjdf ksj fds jdf');
+
+});
+
+$botman->listen();
+
